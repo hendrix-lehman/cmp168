378d3d3bf33b2558aafbbc21b946d39e0af10563
Hendrix Tavarez <htavarez@gmail.com>
2024-02-16 10:26:24 -0500

    objects

--- Objects/App.java
+++ Objects/App.java
@@ -2,6 +2,8 @@ class App {
 
   public static void main(String[] args) {
 
+    // the way we create an instance of a class
+    // is by using the *new* keyword and calling its constructor
     Person person = new Person("John");
     System.out.println("This person's name is " + person.getName());
     person.walk();
@@ -15,8 +17,28 @@ class App {
     System.out.println("This person's name is " + person2.getName());
     person2.walk();
 
-    Person person3 = new Person("Bob", true);
+    Person person3 = new Person("Bobby", true);
     System.out.println("This person's name is " + person3.getName());
+
+    // accessing a constant
+    System.out.println("The maximum age is " + person3.MAX_AGE);
+
+    // accessing a constant Math.PI
+    System.out.println("The value of PI is " + Math.PI);
+
+    // Object is the root of the class hierarchy
+    // Every class has Object as a superclass
+    Object obj = new Object();
+    // I can call the toString method on any object
+    System.out.println(obj.toString());
+    System.out.println("is obj equal to person? " + obj.equals(person));
+
+    // I can call the toString method on any object
+    System.out.println(person.toString());
+    System.out.println(person2.toString());
+    System.out.println(person3.toString());
+    person.setName(person2.getName());
+    System.out.println("is person equal to person2? " + person.equals(person2));
 
   }
 
--- Objects/Person.java
+++ Objects/Person.java
@@ -3,8 +3,9 @@ class Person {
 
   // ATTRIBUTES
   // allows you to hide information
-  private String name;;
+  private String name;
   private boolean hasShoes = false;
+  public final int MAX_AGE = 100;
 
   // CONSTRUCTORS
   // creating an instance via a constructor
@@ -24,6 +25,12 @@ class Person {
     this.name = name;
     this.hasShoes = hasShoes;
   }
+
+  // methods and constructors are NOT the same thing
+  // constructors are used to create an instance of a class
+  // methods are used to describe the behavior of the object
+  // 1. constructors have the same name as the class
+  // 2. constructors do not have a return type
 
   // GETTERS AND SETTERS
   // getter method (accessor)
@@ -48,9 +55,30 @@ class Person {
   // that describe the behavior of the object
   public void walk() {
     if (this.hasShoes) {
-      System.out.println(name + " is walking");
+      System.out.println(name + " is walking with shoes on");
     } else {
-      System.out.println(name + " is barefoot");
+      System.out.println(name + " is walking barefoot");
     }
+  }
+
+  // OVERRIDING METHODS
+  // toString is a method that is inherited from the Object class
+  // we can override it to provide a more meaningful representation of the object
+  @Override
+  public String toString() {
+    return getClass().getName() + ": " + name;
+  }
+
+  // equals is a method that is inherited from the Object class
+  // we can override it to provide a more meaningful comparison of the object
+  @Override
+  public boolean equals(Object abc) {
+    if (abc == null)
+      return false;
+    if (abc instanceof Person) {
+      Person p = (Person) abc;
+      return this.name.equals(p.name);
+    }
+    return false;
   }
 }
